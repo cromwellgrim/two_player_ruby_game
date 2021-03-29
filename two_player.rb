83e0ad2bb9388bc3c40861cf32b0e654f81dce1b
cromwellgrim <rogers.dylan@protonmail.com>
2021-03-29 19:19:17 +0000

    planning files uploaded

---
+++ two_player.rb
@@ -0,0 +1,7 @@
+# player 1 puts a question
+# player 2 answers the question
+# if answered wrong player loses a life
+# prints ------- NEW TURN -------
+# once a player loses all lives the game ends
+
+class Player
